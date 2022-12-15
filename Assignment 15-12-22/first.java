e36c2bb6a590afc72ac0ff38f903270eb9dc4f52
Anu3012 <98791964+Anu3012@users.noreply.github.com>
2022-12-15 12:34:05 +0530

    Create first.java

---
+++ Assignment 15-12-22/first.java
@@ -0,0 +1,17 @@
+public class Anu {
+
+	public static void main(String[] args) {
+		 
+	        int i;
+	        for (i=0;i<5;i++)
+	        {
+	        System.out.println("Hello");
+	    }
+	    System.out.println();
+	    for (i=5;i>0;i--)
+	    {
+	    System.out.println("World");
+	    }
+	}
+	}
+
