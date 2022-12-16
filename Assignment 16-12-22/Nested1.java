197e9136c3d9b6e3e58de7ef6b6e96be9801e4b9
Anu3012 <98791964+Anu3012@users.noreply.github.com>
2022-12-16 13:28:29 +0530

    Create Nested1.java

---
+++ Assignment 16-12-22/Nested1.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class Nested1 {
+
+	public static void main(String[] args) {
+		//loop for i
+		for(int i=1;i<=3;i++)
+		{
+			//loop for j
+			for(int j=1;j<=3;j++) 
+			{
+				System.out.println(i+" "+j);
+			}
+		}//end of j
+
+	}//end of i
+
+}
